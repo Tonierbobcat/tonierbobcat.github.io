1e25df745dc3d443f5110865f3695d9a143e40fd
Logan <tonierbobcat966@gmail.com>
2025-04-16 05:49:56 -0700

    major changes

--- tonierbobcat/assets/js/showcase.js
+++ tonierbobcat/assets/js/showcase.js
@@ -2,38 +2,32 @@ const workItems = [
     {
         image: "images/fulls/01.jpg",
         thumbnail: "images/thumbs/01.jpg",
-        title: "Magna sed consequat tempus",
-        description: "Lorem ipsum dolor sit amet nisl sed nullam feugiat."
+        title: "(Plugin Development) Minecraft Party System",
+        description: "An advanced groundbreaking party system for minecraft that allows players to create parties with players and NPCs."
     },
     {
         image: "images/fulls/02.jpg",
         thumbnail: "images/thumbs/02.jpg",
-        title: "Ultricies lacinia interdum",
-        description: "Lorem ipsum dolor sit amet nisl sed nullam feugiat."
+        title: "(Plugin Development) Labs Minigame",
+        description: "Similar to LabsMC where players get spawned on a platform and for the duration of the game random events fire until there is one player left."
     },
     {
         image: "images/fulls/03.jpg",
         thumbnail: "images/thumbs/03.jpg",
-        title: "Tortor metus commodo",
-        description: "Lorem ipsum dolor sit amet nisl sed nullam feugiat."
+        title: "(Software Development) Aseprite Sprite Sheet Convert For LibGDX",
+        description: "A simple software that converts the sprite sheet json into .atlas files for the LibGDX framework for java."
     },
     {
         image: "images/fulls/04.jpg",
         thumbnail: "images/thumbs/04.jpg",
-        title: "Quam neque phasellus",
-        description: "Lorem ipsum dolor sit amet nisl sed nullam feugiat."
+        title: "(Plugin Development) Minecraft AI Companion Addon",
+        description: "Allows players to have their own follower is intergrated with an LLM so players can chat with their companion. It is aware of the players surroundings and the items in their inventory."
     },
     {
         image: "images/fulls/05.jpg",
         thumbnail: "images/thumbs/05.jpg",
-        title: "Nunc enim commodo aliquet",
-        description: "Lorem ipsum dolor sit amet nisl sed nullam feugiat."
-    },
-    {
-        image: "images/fulls/06.jpg",
-        thumbnail: "images/thumbs/06.jpg",
-        title: "Risus ornare lacinia",
-        description: "Lorem ipsum dolor sit amet nisl sed nullam feugiat."
+        title: "(Game Development) UNNAMED Plant Game",
+        description: "A game where you are a botanist in Japan and you go out and discover plants to plant into your garden. As you complete the main quest you unlock decorations that you can decorate your garden with."
     }
 ];
 
